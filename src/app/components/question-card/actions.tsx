2c42449176d9dd5cd6ff993469226a996aff9741
DaeHee Lee <lee111dae11@gmail.com>
2023-10-27 22:49:48 +0900

    리팩토링:퀴즈 관련 액션 컴포넌트 리팩토링

--- src/app/components/question-card/actions.tsx
+++ src/app/components/question-card/actions.tsx
@@ -1,21 +1,36 @@
 import { useAtom } from "jotai";
-import { useFormContext } from "react-hook-form";
+import { useFormContext, useWatch } from "react-hook-form";
 import { questionNumberAtom } from "~/app/atoms";
 import { FormPayload } from "~/app/types";
 
-export function Actions() {
-  const { getValues } = useFormContext<FormPayload>();
-  const [questionNumber, setQuestionNumber] = useAtom(questionNumberAtom);
-  const isFirst = questionNumber === 0;
-  const isLast = questionNumber === 9;
+const MIN_QUESTION_NUMBER = 0;
+const MAX_QUESTION_NUMBER = 9;
 
+export function Actions() {
+  const { control, getValues } = useFormContext<FormPayload>();
+
+  const [questionNumber, setQuestionNumber] = useAtom(questionNumberAtom);
+  const isFirst = questionNumber === MIN_QUESTION_NUMBER;
+  const isLast = questionNumber === MAX_QUESTION_NUMBER;
+
+  // 값이 업데이트 된 다음에 useWatch가 실행되면 올바르지 않은 값이 반환될 수 있어
+  // getValues를 함께 사용하였습니다.
+  // 참고: https://react-hook-form.com/docs/usewatch
+  useWatch({
+    control,
+    name: `${questionNumber}`,
+  });
   const value = getValues(`${questionNumber}`);
 
-  const nextQuestion = () => {
-    setQuestionNumber((prev) => Math.min(prev + 1, 9));
-  };
+  const [buttonText, buttonType] = isLast
+    ? (["제출", "submit"] as const)
+    : (["다음", "button"] as const);
+
   const prevQuestion = () => {
     setQuestionNumber((prev) => Math.max(prev - 1, 0));
+  };
+  const nextQuestion = () => {
+    setQuestionNumber((prev) => Math.min(prev + 1, 9));
   };
 
   return (
@@ -29,12 +44,12 @@ export function Actions() {
         이전
       </button>
       <button
-        type={isLast ? "submit" : "button"}
+        type={buttonType}
         className="btn"
         onClick={isLast ? undefined : nextQuestion}
         disabled={value == null}
       >
-        {isLast ? "제출" : "다음"}
+        {buttonText}
       </button>
     </div>
   );
